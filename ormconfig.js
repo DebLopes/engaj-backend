caf8dc656f7cf14123cd5c9bab9625cf557fb5b6
Debora Lopes <deborasantoslop@gmail.com>
2021-04-25 20:14:33 -0300

    ssl true

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,27 @@
-console.log('process.env.DATABASE_URL >>', process.env.DATABASE_URL);
 console.log('process.env.DATABASE_URL >>', process.env.ENVIRONMENT);
 
 module.exports = {
   "name": "default",
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "ssl": true,
+  "extra": {
+    "ssl": {
+      "rejectUnauthorized": false,
+    },
+  },
   "entities": [
     process.env.ENVIRONMENT == "desenvolvimento" ?
-    "./src/models/*.ts" :
-    "./dist/models/*.js"
+      "./src/models/*.ts" :
+      "./dist/models/*.js"
   ],
   "migrations": [
     process.env.ENVIRONMENT == "desenvolvimento" ?
-    "./src/database/migrations/*.ts" :
-    "./dist/database/migrations/*.js"
+      "./src/database/migrations/*.ts" :
+      "./dist/database/migrations/*.js"
   ],
   "cli": {
     "migrationsDir": "./src/database/migrations",
-    "entitiesDir":"./src/models",
+    "entitiesDir": "./src/models",
   }
 }
